e5e00f2b141776ef92f0315cc9ee47c4252ff865
Zac Crawford <zwcrawford@gmail.com>
2018-11-28 12:50:33 -0600

    functions are hard...

---
+++ battle.js
@@ -0,0 +1,16 @@
+let bandNumber = 1;
+
+const takeNumber = function (bandName) {
+    /*
+    Write your awesome code here. See comments
+    below for what should be returned.
+    */
+   for (bandNumber = 1; bandNumber <= 1; bandNumber++) {
+       console.log(`${bandNumber}. ${bandName}`);
+    }
+}
+const scum = takeNumber("Galactic Scum");
+// console.log(scum); -- This should print "1. Galactic Scum" in the console
+const under = takeNumber("Underdogs");
+// console.log(under); -- This should print "2. Underdogs" in the console
+
